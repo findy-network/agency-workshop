aac4d9bbea23aa3b02726a05fbd7756e70a06c02
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-04-12 16:27:04 +0300

    Add solution for task 0.

--- track2.1-ts/app/src/index.ts
+++ track2.1-ts/app/src/index.ts
@@ -1,9 +1,36 @@
 import express, { Express, Request, Response } from 'express';
+import { createAcator, openGRPCConnection } from '@findy-network/findy-common-ts'
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+const setupAgentConnection = async () => {
+  const acatorProps = {
+    authUrl: process.env.FCLI_URL!,
+    authOrigin: process.env.FCLI_ORIGIN!,
+    userName: process.env.FCLI_USER!,
+    key: process.env.FCLI_KEY!,
+  }
+  // Create authenticator
+  const authenticator = createAcator(acatorProps)
+
+  const serverAddress = process.env.AGENCY_API_SERVER!
+  const certPath = process.env.FCLI_TLS_PATH!
+  const grpcProps = {
+    serverAddress,
+    serverPort: parseInt(process.env.AGENCY_API_SERVER_PORT!, 10),
+    // NOTE: we currently assume that we do not need certs for cloud installation
+    // as the cert is issued by a trusted issuer
+    certPath: serverAddress === 'localhost' ? certPath : ''
+  }
+
+  // Open gRPC connection to agency using authenticator
+  return openGRPCConnection(grpcProps, authenticator)
+}
+
 const runApp = async () => {
+  await setupAgentConnection()
+
   app.get('/greet', async (req: Request, res: Response) => {
     throw 'IMPLEMENT ME!'
   });
