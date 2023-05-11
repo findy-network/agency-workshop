971dff21961ca62ad600d7c41de23768590f5c71
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-05-11 16:12:41 +0300

    Add solution for task 1.

--- track2.2-go/app/main.go
+++ track2.2-go/app/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/findy-network/agency-workshop/agent"
+	"github.com/findy-network/agency-workshop/handlers"
+	agency "github.com/findy-network/findy-common-go/grpc/agency/v1"
 	"github.com/lainio/err2"
 	"github.com/lainio/err2/try"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
-type app struct{}
+type app struct {
+	agencyClient *agent.AgencyClient
+	// Store greeter handler to app state
+	greeter *handlers.Greeter
+}
 
 func (a *app) homeHandler(response http.ResponseWriter, r *http.Request) {
 	defer err2.Catch(func(err error) {
@@ -26,7 +37,10 @@ func (a *app) greetHandler(response http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		http.Error(response, err.Error(), http.StatusInternalServerError)
 	})
-	try.To1(response.Write([]byte("IMPLEMENT ME")))
+	// Create HTML payload
+	_, html := try.To2(createInvitationPage(a.agencyClient.AgentClient, "Greet"))
+	// Render HTML
+	try.To1(response.Write([]byte(html)))
 }
 
 // Show pairwise invitation. Once connection is established, issue credential.
@@ -47,15 +61,61 @@ func (a *app) verifyHandler(response http.ResponseWriter, r *http.Request) {
 	try.To1(response.Write([]byte("IMPLEMENT ME")))
 }
 
+func createInvitationPage(
+	agentClient agency.AgentServiceClient,
+	header string,
+) (html, invitationID string, err error) {
+	defer err2.Handle(&err)
+
+	// Agency API call for creating the DIDComm connection invitation
+	res := try.To1(agentClient.CreateInvitation(
+		context.TODO(),
+		// Whichever name we want to expose from ourselves to the other end
+		&agency.InvitationBase{Label: os.Getenv("FCLI_USER")},
+	))
+
+	var invitationMap map[string]any
+	try.To(json.Unmarshal([]byte(res.GetJSON()), &invitationMap))
+
+	url := res.URL
+	log.Printf("Created invitation\n %s\n", url)
+
+	// Convert invitation string to QR code
+	png, err := qrcode.Encode(url, qrcode.Medium, 512)
+	imgSrc := "data:image/png;base64," + base64.StdEncoding.EncodeToString([]byte(png))
+
+	// Create HTML payload
+	html = `<html>
+      <h1>` + header + `</h1>
+      <p>Read the QR code with the wallet application:</p>
+      <img src="` + imgSrc + `"/>
+      <p>or copy-paste the invitation:</p>
+      <textarea onclick="this.focus();this.select()" readonly="readonly" rows="10" cols="60">` +
+		url + `</textarea></html>`
+
+	// Return invitation id and the HTML payload
+	return invitationMap["@id"].(string), html, nil
+}
+
 func main() {
 	defer err2.Catch(func(err error) {
 		log.Fatal(err)
 	})
 
 	// Login agent
-	_ = try.To1(agent.LoginAgent())
+	agencyClient := try.To1(agent.LoginAgent())
 
-	myApp := app{}
+	// Create handlers
+	myApp := app{
+		agencyClient: agencyClient,
+		greeter:      handlers.NewGreeter(agencyClient.Conn),
+	}
+
+	// Start listening
+	myApp.agencyClient.Listen([]agent.Listener{
+		// Greeter handles the greeting logic
+		myApp.greeter,
+	})
 
 	router := http.NewServeMux()
 
--- track2.3-kt/app/src/main/kotlin/fi/oplab/findyagency/workshop/WorkshopApplication.kt
+++ track2.3-kt/app/src/main/kotlin/fi/oplab/findyagency/workshop/WorkshopApplication.kt
@@ -1,5 +1,7 @@
 package fi.oplab.findyagency.workshop
 
+import kotlinx.serialization.decodeFromString
+import org.findy_network.findy_common_kt.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,12 +13,80 @@ fun main(args: Array<String>) {
   runApplication<WorkshopApplication>(*args)
 }
 
+@kotlinx.serialization.Serializable data class InvitationData(
+  @kotlinx.serialization.SerialName("@id") val id: String
+)
+
 @RestController
 class AppController {
   val agent = Agent()
+  val greeter = Greeter(agent.connection)
+
+  init {
+    val listeners = ArrayList<Listener>()
+    listeners.add(greeter)
+    // Start listening to agent notifications 
+    agent.listen(listeners)
+  }
+
+  fun createInvitationPage(header: String): Pair<String, String> {
+
+    // Create invitation to our agent
+    val invitation = createInvitation()
+
+    // Parse invitation id
+    val data = kotlinx.serialization.json.Json { ignoreUnknownKeys = true }
+      .decodeFromString<InvitationData>(invitation.getJSON())
+    println("Created invitation with id ${data.id}")
+
+    val imgSrc = "data:image/png;base64," + createQRCode(invitation.url)
+    val html = """<html>
+    <h1>${header}</h1>
+    <p>Read the QR code with the wallet application:</p>
+    <img src="${imgSrc}"/>
+    <p>or copy-paste the invitation:</p>
+    <textarea onclick="this.focus();this.select()" readonly="readonly" rows="10" cols="60">${invitation.url}</textarea></html>"""
+
+    // return both html page and invitation id
+    return Pair(html, data.id)
+  }
+
+  fun createInvitation(): Invitation = kotlinx.coroutines.runBlocking {
+    // Use as label whichever name we want to expose from ourselves to the other end
+    agent.connection.agentClient.createInvitation(label = System.getenv("FCLI_USER"))
+  }
+
+  // Utility for converting string to QR code
+  fun createQRCode(value: String): String {
+    val writer = com.google.zxing.qrcode.QRCodeWriter()
+    val bitMatrix = writer.encode(value, com.google.zxing.BarcodeFormat.QR_CODE, 512, 512)
+    val width = bitMatrix.width
+    val height = bitMatrix.height
+    val bitmap = java.awt.image.BufferedImage(
+      width,
+      height,
+      java.awt.image.BufferedImage.TYPE_USHORT_565_RGB
+    )
+    for (x in 0 until width) {
+      for (y in 0 until height) {
+        bitmap.setRGB(
+          x,
+          y,
+          if (bitMatrix.get(x, y)) java.awt.Color.BLACK.getRGB() else java.awt.Color.WHITE.getRGB()
+        )
+      }
+    }
+    val out = java.io.ByteArrayOutputStream()
+    javax.imageio.ImageIO.write(bitmap, "PNG", out)
+    return java.util.Base64.getEncoder().encodeToString(out.toByteArray())
+  }
+
 
   @GetMapping("/") fun index(): String = "Kotlin example"
-  @GetMapping("/greet") fun greet(): String = "IMPLEMENT ME"
+  @GetMapping("/greet") fun greet(): String {
+    val (html) = createInvitationPage("Greet")
+    return html
+  }
   @GetMapping("/issue") fun issue(): String = "IMPLEMENT ME"
   @GetMapping("/verify") fun verify(): String = "IMPLEMENT ME"
 }
